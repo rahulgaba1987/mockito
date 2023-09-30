ec4542d7129a6b6a39a8be127aa604c2723b904c
Rahul Gaba <rahulgaba4u1987@gmail.com>
2023-09-30 23:17:21 +0530

    Service Layer JUNIT Testing

--- src/main/java/com/boot/service/EmployeeService.java
+++ src/main/java/com/boot/service/EmployeeService.java
@@ -1,9 +1,19 @@
 package com.boot.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.boot.entity.EmployeeEntity;
 
 public interface EmployeeService 
 {
 	EmployeeEntity saveEmployee(EmployeeEntity employeeEntity);
+	
+	List<EmployeeEntity> getAllEmployees();
+	
+	Optional<EmployeeEntity>  getEmployeeById(Long employeeId);
+	
+	EmployeeEntity updateEmployee(EmployeeEntity updateEmployee);
+	public void deleteEmployee(Long employeeId);
 
 }
--- src/main/java/com/boot/serviceimplementation/EmployeeServiceImplementation.java
+++ src/main/java/com/boot/serviceimplementation/EmployeeServiceImplementation.java
@@ -1,5 +1,6 @@
 package com.boot.serviceimplementation;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,12 @@ import com.boot.service.EmployeeService;
 public class EmployeeServiceImplementation implements EmployeeService
 {
 
-	@Autowired
 	private EmployeeRepository employeeRepository;
+	
+	public EmployeeServiceImplementation(EmployeeRepository employeeRepository ) 
+	{
+		this.employeeRepository=employeeRepository;
+	}
 	
 	@Override
 	public EmployeeEntity saveEmployee(EmployeeEntity employeeEntity) 
@@ -31,8 +36,59 @@ public class EmployeeServiceImplementation implements EmployeeService
 			return employeeRepository.save(employeeEntity);
 			
 		}
+	}
+
+	@Override
+	public List<EmployeeEntity> getAllEmployees() 
+	{
+			return employeeRepository.findAll();
+	}
+
+	@Override
+	public Optional<EmployeeEntity> getEmployeeById(Long employeeId) 
+	{
+		Optional<EmployeeEntity> employeeObj = employeeRepository.findById(employeeId);
 		
+		if(employeeObj.isPresent())
+		{
+			return employeeObj;
+		}
+		else
+		{
+			throw new ResourceNotFoundException("Employee obj does not exist with employee ID  "+employeeId);
+		}
 		
+	}
+
+	@Override
+	public EmployeeEntity updateEmployee(EmployeeEntity updateEmployee) 
+	{
+		
+        Optional<EmployeeEntity> employeeObj = employeeRepository.findById(updateEmployee.getEmployeeId());
+		
+		if(employeeObj.isPresent())
+		{
+			return employeeRepository.save(updateEmployee);
+		}
+		else
+		{
+			throw new ResourceNotFoundException("Employee obj does not exist with employee ID  "+updateEmployee.getEmployeeId());
+		}
+	}
+
+	@Override
+	public void deleteEmployee(Long employeeId)
+	{
+		 Optional<EmployeeEntity> employeeObj = employeeRepository.findById(employeeId);
+			
+		 if(employeeObj.isPresent())
+		 {
+			  employeeRepository.deleteById(employeeId);
+		 }
+		 else
+		 {
+			 throw new ResourceNotFoundException("Employee obj does not exist with employee ID  "+employeeId);
+		 }
 		
 	}
 
