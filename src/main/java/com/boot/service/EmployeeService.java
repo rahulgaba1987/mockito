f83c18acc06dfcf873b1e8235ca831989e52f2ad
Rahul Gaba <rahulgaba4u1987@gmail.com>
2023-10-01 10:37:50 +0530

    update log file

--- src/main/java/com/boot/service/EmployeeService.java
+++ src/main/java/com/boot/service/EmployeeService.java
@@ -1,27 +1,23 @@
 package com.boot.service;
 
-<<<<<<< HEAD
+
 import java.util.List;
 import java.util.Optional;
-=======
->>>>>>> 80cd25995981557a974305fb8a34ead9478ecf9d
+
 
 import com.boot.entity.EmployeeEntity;
 
 public interface EmployeeService 
 {
 	EmployeeEntity saveEmployee(EmployeeEntity employeeEntity);
-<<<<<<< HEAD
-	
+
 	List<EmployeeEntity> getAllEmployees();
 	
 	Optional<EmployeeEntity>  getEmployeeById(Long employeeId);
 	
 	EmployeeEntity updateEmployee(EmployeeEntity updateEmployee);
 	public void deleteEmployee(Long employeeId);
-=======
-public interface EmployeeService {
 
->>>>>>> 80cd25995981557a974305fb8a34ead9478ecf9d
+
 
 }
